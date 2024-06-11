e0f822cbad4e7a825e03eb5ccb73490491eded6f
shakildanil <dan.sushencev@gmail.com>
2024-06-11 17:04:01 +0300

    убрал <script/> из onMounted

--- app/src/components/TelegramLogin.vue
+++ app/src/components/TelegramLogin.vue
@@ -12,25 +12,16 @@
     setup() {
       onMounted(() => {
         const tg = window.Telegram.WebApp;
-        
+  
         tg.ready();
   
         tg.MainButton.setText('Войти через Telegram');
         tg.MainButton.show();
   
         tg.MainButton.onClick(() => {
-          const script = document.createElement('script');
-          script.async = true;
-          script.src = "https://telegram.org/js/telegram-widget.js?22";
-          script.setAttribute('data-telegram-login', 'channelstech_dev_bot');
-          script.setAttribute('data-size', 'medium');
-          script.setAttribute('data-onauth', 'onTelegramAuth(user)');
-          document.body.appendChild(script);
+          const telegramLoginUrl = "https://oauth.telegram.org/auth?bot_id=your_bot_id&origin=https://yourdomain.com";
+          window.location.href = telegramLoginUrl;
         });
-        
-        (window as any).onTelegramAuth = (user: any) => {
-          alert(`Logged in as ${user.first_name} ${user.last_name} (${user.id}${user.username ? ', @' + user.username : ''})`);
-        };
       });
   
       return {};
