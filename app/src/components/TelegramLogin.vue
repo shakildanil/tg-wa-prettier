78ced2a656f065b18382a22a4070b8b406e6a433
shakildanil <dan.sushencev@gmail.com>
2024-06-11 17:36:00 +0300

    убрал <script/> из onMounted

--- app/src/components/TelegramLogin.vue
+++ app/src/components/TelegramLogin.vue
@@ -1,35 +1,41 @@
 <template>
     <div>
-      <!-- Telegram login будет осуществляться через MainButton -->
+        <!-- Telegram login будет осуществляться через MainButton -->
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, onMounted } from 'vue';
-  
-  export default defineComponent({
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+
+export default defineComponent({
     name: 'TelegramLogin',
     setup() {
-      onMounted(() => {
-        const tg = window.Telegram.WebApp;
-  
-        tg.ready();
-  
-        tg.MainButton.setText('Войти через Telegram');
-        tg.MainButton.show();
-  
-        tg.MainButton.onClick(() => {
-          const telegramLoginUrl = "https://oauth.telegram.org/auth?bot_id=your_bot_id&origin=https://yourdomain.com";
-          window.location.href = telegramLoginUrl;
+        onMounted(() => {
+            const tg = window.Telegram.WebApp;
+
+            tg.ready();
+
+            tg.MainButton.setText('Войти через Telegram');
+            tg.MainButton.show();
+
+            // tg.MainButton.onClick(() => {
+            //     const telegramLoginUrl = "https://oauth.telegram.org/auth?bot_id=7293138530&origin=https://yourdomain.com";
+            //     window.location.href = telegramLoginUrl;
+            // });
+
+            tg.MainButton.onClick(() => {
+                const botId = '7293138530'; // Замените на ваш bot_id
+                const origin = window.location.origin;
+                const telegramLoginUrl = `https://oauth.telegram.org/auth?bot_id=${botId}&origin=${origin}`;
+                window.location.href = telegramLoginUrl;
+            });
         });
-      });
-  
-      return {};
+
+        return {};
     },
-  });
-  </script>
-  
-  <style scoped>
-  /* Дополнительные стили, если нужны */
-  </style>
-  
+});
+</script>
+
+<style scoped>
+/* Дополнительные стили, если нужны */
+</style>
